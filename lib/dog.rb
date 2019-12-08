e0cc9e4ee08fe5a9e3e827958911681a934c16b6
Stela Capsa <stela.capsa@gmail.com>
2019-12-08 20:00:57 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,34 @@
+require "pry"
+class Dog
+# def initialize(name)
+  @name = name
+# end
+
+def name 
+  @name
+ end
+
+ def name=(new_name)
+    @name = new_name
+  end
+  
+  fido = Dog.new
+  fido.name = ("Fido")
+  puts fido.name
+end
+  
+  
+  class Dog
+  
+  def breed 
+    @breed 
+  end 
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  
+  snoopy = Dog.new 
+  snoopy.breed = ("Beagle")
+  puts snoopy.breed
+ end
---
+++ lib/person.rb
@@ -0,0 +1,35 @@
+class Person
+   @name = name 
+
+ 
+ def name 
+   @name 
+ end 
+ 
+ def name=(new_name)
+   @name = new_name
+ end 
+ 
+ beyonce = Person.new 
+ beyonce.name = ("Beyonce")
+ puts beyonce.name
+ end
+ 
+ 
+ 
+ 
+class Person
+  
+   
+   def job
+     @job
+   end
+   
+   def job=(new_job)
+     @job = new_job
+   end 
+    
+  beyonce = Person.new 
+  beyonce.job = ("Singer")
+  puts beyonce.job 
+ end
